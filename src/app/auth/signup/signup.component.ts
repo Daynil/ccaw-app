9cb3af551ca006d732773552402e32e084f2c410
Jennifer Bland <ratracegrad@gmail.com>
2016-08-20 19:24:02 -0400

    Added router for signup page and added bootstrap for styling

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { ROUTER_DIRECTIVES, Router } from '@angular/router';
 
 import { TransitionService } from '../../shared/transition.service';
 import { ToastComponent } from '../../shared/toast.component';
@@ -8,13 +9,14 @@ import { ToastComponent } from '../../shared/toast.component';
                selector: 'signup',
                templateUrl: 'signup.component.html',
                styleUrls: ['signup.component.css'],
-               directives: [ToastComponent]
+               directives: [ToastComponent, ROUTER_DIRECTIVES]
            })
 export class SignupComponent implements OnInit {
 
     @ViewChild('toast') toast: ToastComponent;
 
-    constructor(private transitionService: TransitionService) { }
+    constructor(private transitionService: TransitionService,
+                private router: Router) { }
 
     ngOnInit() {
         this.transitionService.transition();
