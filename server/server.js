5f4f57db9afa2659aa0985ee2826133e77bc941a
Jennifer Bland <ratracegrad@gmail.com>
2016-08-21 16:29:09 -0400

    ls

--- server/server.js
+++ server/server.js
@@ -1,319 +1,70 @@
 'use strict';
 
+/** Get all Modules we will need **/
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const compress = require('compression');
 const _ = require('lodash');
+const passport = require('passport');
+const expressSession = require('express-session');
 
 const app = express();
+
+/** Define Variables  **/
+let production = process.env.NODE_ENV === 'production';
+let port = process.env.PORT || 3000;
 
 // Load local environment variables in development
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').load();
 }
-let production = process.env.NODE_ENV === 'production';
+
+/*  Configure Connection to MongoDB  **/
+const mongoose = require('mongoose');
+// TODO we need to change the below to use the mlab database used with heroku
+let mongoURI = process.env.MONGO_URI || 'mongodb://localhost/ccaw-app';
+mongoose.connect(mongoURI);
 
 if (process.env.SEED_DB && process.env.SEED_DB==='true') {
   require('./seed');
 }
 
-// Database
-const mongoose = require('mongoose');
-const Conference = require('./models/conference');
-const Speaker = require('./models/speaker');
-const Session = require('./models/session');
-const passport = require('passport');
-const expressSession = require('express-session');
-
-let mongoURI = process.env.MONGO_URI || 'mongodb://localhost/ccaw-app';
-
-mongoose.connect(mongoURI);
-function updateActiveConfs(activeConf) {
-  // If no active conf passed, make all confs inactive
-  if (activeConf === null) activeConf = {title: ''};
-  let savePromise = new Promise((resolve, reject) => {
-    Conference
-      .find({})
-      .exec()
-      .then(conferences => {
-        let allSavesSuccessful = true;
-        for (let i = 0; i < conferences.length; i++) {
-          let serverConf = conferences[i];
-          serverConf.lastActive = serverConf.title === activeConf.title;
-          serverConf.save(err => {
-            console.log('conf saved');
-            if (err) {
-              console.log(err);
-              allSavesSuccessful = false;
-            }
-          });
-        }
-        resolve(allSavesSuccessful);
-      });
-  });
-  return savePromise;
-}
-
 /** True = get response details on served node modules **/
 let verboseLogging = false;
 
-/** Gzip files in production **/
+/**  Configure middleware  **/
 if (production) {
   app.use(compress());
 }
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(expressSession({
-  secret: 'cookie_secret',
-  proxy: true,
-  resave: true,
-  saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use(morgan('dev', {
   skip: (req, res) => {
     if (verboseLogging) return false;
     else return req.baseUrl === '/scripts';
   }
 }));
-
 app.use( express.static( path.join(__dirname, '../dist') ));
-
 app.use('/scripts', express.static( path.join(__dirname, '../node_modules') ));
 app.use('/app', express.static( path.join(__dirname, '../dist/app') ));
 app.use('/public', express.static( path.join(__dirname, '../public') ));
 
-app.get('/api/getallconferences', (req, res) => {
-  Conference
-    .find({})
-    .exec()
-    .then(conferences => {
-      res.status(200).json(conferences);
-    });
-});
+/** Configure Passport **/
+app.use(expressSession({
+                         secret: 'cookie_secret',
+                         proxy: true,
+                         resave: true,
+                         saveUninitialized: true
+                       }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/api/getallspeakers', (req, res) => {
-  Speaker
-    .find({})
-    .exec()
-    .then(speakers => {
-      res.status(200).json(speakers);
-    })
-});
+/** Get Routes  **/
+require('../routes/index.js')(app, passport);
 
-app.get('/api/getallsessions', (req, res) => {
-  Session
-    .find({})
-    .exec()
-    .then(sessions => {
-      res.status(200).json(sessions);
-    })
-});
-
-app.post('/api/createconference', (req, res) => {
-  let conf = req.body;
-  console.log('conf:', conf);
-  updateActiveConfs(null).then(saveSuccess => {
-    if (saveSuccess) {
-      let newConf = new Conference();
-      newConf.lastActive = true;
-      newConf.title = conf.title;
-      newConf.dateRange = {
-        start: conf.dateRange.start,
-        end: conf.dateRange.end
-      };
-      newConf.save(err => {
-        if (err) {
-          console.log(err);
-          res.status(500).json({message: 'Conference save error'});
-        }
-        else res.status(200).json({message: 'Conference created'});
-      });
-    } else res.status(500).json({message: 'Conferences updating error'});
-  });
-});
-
-app.post('/api/changeactiveconf', (req, res) => {
-  let conf = req.body;
-
-  updateActiveConfs(conf).then(saveSuccess => {
-    if (saveSuccess) res.status(200).json({message: 'Conferences update'});
-    else res.status(500).json({message: 'Conferences updating error'});
-  });
-});
-
-app.post('/api/changetimeslot', (req, res) => {
-  let conf = req.body;
-
-  Conference
-    .findOne({ title: conf.title })
-    .exec()
-    .then(serverConf => {
-      serverConf.days = conf.days;
-      serverConf.save(err => {
-        if (err) res.status(500).json({message: 'Conference save error'});
-        else res.status(200).json(serverConf);
-      });
-    });
-});
-
-app.post('/api/addRoom', (req, res) => {
-  let conf = req.body;
-
-  Conference
-    .findOne({title: conf.title})
-    .exec()
-    .then(serverConf => {
-      serverConf.rooms = conf.rooms;
-      serverConf.save(err => {
-        if (err) res.status(500).json({ message: 'Conference room save error'});
-        else res.status(200).json({message: 'Conference room saved'});
-      });
-  });
-});
-
-app.post('/api/deleteRoom', (req, res) => {
-  let conf = req.body;
-
-  Conference
-    .findOne({title: conf.title})
-    .exec()
-    .then(serverConf => {
-      serverConf.rooms = conf.rooms;
-      serverConf.save(err => {
-        if (err) res.status(500).json({ message: 'Conference room save error'});
-        else res.status(200).json({message: 'Conference room saved'});
-      });
-  });
-});
-
-app.post('/api/updateconference', (req, res) => {
-  let currentTitle = req.body.currentTitle;
-  let conf = req.body.conference;
-
-  Conference
-    .findOne({ title: currentTitle })
-    .exec()
-    .then(serverConf => {
-      serverConf.title = conf.title;
-      serverConf.dateRange = conf.dateRange;
-      serverConf.save(err => {
-        if (err) res.status(500).json({message: 'Conference save error'});
-        else res.status(200).json({message: 'Conference saved'});
-      });
-    });
-});
-
-app.post('/api/updatespeaker', (req, res) => {
-  let speaker = req.body;
-  // Existing speakers have an id
-  if (speaker._id) {
-    Speaker
-      .findById(speaker._id)
-      .exec()
-      .then(serverSpeaker => {
-        if (serverSpeaker === null) {
-          console.log('Speaker not found');
-          res.status(500).json({message: 'Speaker not found'});
-        } else {
-          console.log('found existing speaker');
-          _.merge(serverSpeaker, speaker);
-          serverSpeaker.save(err => {
-            if (err) {
-              console.log(err);
-              res.status(500).json({message: 'Speaker save error'});
-            } else res.status(200).json(serverSpeaker);
-          });
-        }
-    });
-  } else {
-    let newSpeaker = new Speaker({
-      admin: false,
-      password: 'password',
-      status: 'pending',
-      statusNotification: false,
-      adminNotes: ''
-    });
-    _.merge(newSpeaker, speaker);
-    newSpeaker.save(err => {
-      if (err) {
-        console.log(err);
-        res.status(500).json({message: 'Speaker save error'});
-      } else res.status(200).json(newSpeaker);
-    });
-  }
-});
-
-app.post('/api/updatesession', (req, res) => {
-  let session = req.body;
-  // Existing sessions have an id
-  if (session._id) {
-    Session
-      .findById(session._id)
-      .exec()
-      .then(serverSession => {
-        if (serverSession === null) {
-          console.log('Session not found');
-          res.status(500).json({message: 'Session not found'});
-        } else {
-          console.log('found existing session');
-          _.merge(serverSession, session);
-          serverSession.save(err => {
-            if (err) {
-              console.log(err);
-              res.status(500).json({message: 'Session save error'});
-            } else res.status(200).json(serverSession);
-          });
-        }
-    });
-  } else {
-    let newSession = new Session();
-    _.merge(newSession, session);
-    newSession.save(err => {
-      if (err) {
-        console.log(err);
-        res.status(500).json({message: 'Session save error'});
-      } else res.status(200).json(newSession);
-    });
-  }
-});
-
-app.post('/api/updatesessionspeakers', (req, res) => {
-  let session = req.body;
-
-  Session
-    .findById(session._id)
-    .exec()
-    .then(serverSession => {
-      if (serverSession === null) {
-        console.log('Session not found');
-        res.status(500).json({message: 'Session not found'});
-      } else {
-        console.log('found existing session');
-        serverSession.speakers = session.speakers;
-        serverSession.save(err => {
-          if (err) {
-            console.log(err);
-            res.status(500).json({message: 'Session save error'});
-          } else res.status(200).json(serverSession);
-        });
-      }
-  });
-});
-
-/** Pass all non-api routes to front-end router for handling **/
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
-});
-
-let port = process.env.PORT || 3000;
+/**  Start Server  **/
 let server = app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
