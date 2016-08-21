d6c5ffd64efad60b4aae5943a7c1450af0b65e47
Jennifer Bland <ratracegrad@gmail.com>
2016-08-21 13:36:09 -0400

    Added user model that will be used when users login to the server

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,30 @@
-/**
- * Created by jbland on 8/21/16.
- */
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+let UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    provider: {
+        type: String,
+        defaultValue: null
+    }
+});
+
+UserSchema.methods.generateHash = password => {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validatePassword = (enteredPass, actualPassHash) => {
+    return bcrypt.compareSync(enteredPass, actualPassHash);
+};
+
+module.exports = mongoose.model('User', UserSchema);
