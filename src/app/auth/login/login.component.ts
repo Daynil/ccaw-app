9b0a104a11d495cfce0b8123c30518c1949f4e0b
Jennifer Bland <ratracegrad@gmail.com>
2016-08-23 20:12:47 -0400

    Added forms to login page. Use to validate fields and disable login button until both fields are filled out properly

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, Validators } from '@angular/forms';
 import { ROUTER_DIRECTIVES, Router } from '@angular/router';
-
 import { AuthService } from '../../shared/auth.service';
 import { TransitionService } from '../../shared/transition.service';
 import { ToastComponent } from '../../shared/toast.component';
@@ -11,39 +10,38 @@ import { ToastComponent } from '../../shared/toast.component';
                selector: 'login',
                templateUrl: 'login.component.html',
                styleUrls: ['login.component.css'],
-               directives: [ToastComponent, ROUTER_DIRECTIVES]
+               directives: [ToastComponent, ROUTER_DIRECTIVES, REACTIVE_FORM_DIRECTIVES]
            })
 export class LoginComponent implements OnInit {
 
     @ViewChild('toast') toast: ToastComponent;
+
+    email: FormControl;
+    password: FormControl;
+    form: FormGroup;
 
     constructor(private transitionService: TransitionService,
                 private router: Router,
                 private authService: AuthService) { }
 
     ngOnInit() {
+        this.email = new FormControl('', Validators.compose([Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]));
+        this.password = new FormControl('', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(18)]));
+
+        this.form = new FormGroup({
+            'email': this.email,
+            'password': this.password
+        });
+
         this.transitionService.transition();
     }
 
-    doLogin(email: HTMLInputElement, password: HTMLInputElement) {
-        let emailTxt = email.value;
-        let passTxt = password.value;
-        if (emailTxt.length < 1 || passTxt.length < 1) return;
-        this.authService.login(emailTxt, passTxt)
+    doLogin(event) {
+        this.authService.login(this.form.value)
             .then((res: any) => {
-/*                console.log('login res', res);
-                return;
-                if (res.alert === 'no user found') {
-                    this.toast.error('Email or password not found');
-                }
-                else if (res.alert === 'wrong password') {
-                    this.toast.error('Email or password entered incorrectly');
-                }
-                else {*/
                     console.log('login res', res);
                     this.toast.success('Logged in!');
                     this.router.navigate(['/home']);
-/*                }*/
             })
             .catch(err => {
                 console.log(err);
@@ -54,6 +52,7 @@ export class LoginComponent implements OnInit {
                     this.toast.error('Login error, please try again later');
                 }
             });
+        event.preventDefault();
     }
 
 }
