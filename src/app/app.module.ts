0cdbb798cff4264f5b5e492fbfcf81391b6c35e5
Danny Libin <omegasol11@gmail.com>
2016-09-01 18:27:48 -0500

    needed to import reactive forms module too

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { routing } from './app.routing';
@@ -48,6 +48,7 @@ import { TimePipe } from './shared/time.pipe';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     routing
   ],
